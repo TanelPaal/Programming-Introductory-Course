bc1c9c5bfb75a33f8a0f58d4c2e002178452947b
Tanel Paal <tanpaa@taltech.ee>
2023-09-06 21:18:22 +0300

    Try1

---
+++ EX/ex02_beginning/poem.py
@@ -0,0 +1,20 @@
+"""EX02 Poem."""
+
+"""
+Ask for user's inputs and print out a poem in one line using f string and newline (\n)
+
+Example input:
+color = "red"
+objects = "violets"
+activity = "code"
+
+Example output:
+Roses are red,
+violets are blue,
+I love to code
+And so will you!
+"""
+color = input("Enter a color: ")
+objects = input("Enter a plural noun: ")
+activity = input("Enter an activity: ")
+print(f"Roses are {color}, \n{objects} are blue, \nI love to {activity}, \nAnd so will you!")
