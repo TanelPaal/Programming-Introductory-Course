1b385f76c918103ed5c3757cdc1306669d47bd52
Tanel Paal <tanpaa@taltech.ee>
2023-09-07 11:06:59 +0300

    Try1

--- EX/ex02_beginning/atm.py
+++ EX/ex02_beginning/atm.py
@@ -13,29 +13,29 @@ amount = int(input("Enter a sum: "))
 banknotes = 0
 #
 # Your code here
-#Calculate the number of 100€ banknotes needed
+# Calculate the number of 100€ banknotes needed
 if amount >= 100:
     banknotes += amount // 100
     amount = amount % 100
-#Calculate the number of 50€ banknotes needed
+# Calculate the number of 50€ banknotes needed
 if amount >= 50:
     banknotes += amount // 50
     amount = amount % 50
-#Calculate the number of 20€ banknotes needed
+# Calculate the number of 20€ banknotes needed
 if amount >= 20:
     banknotes += amount // 20
     amount = amount % 20
-#Calculate the number of 10€ banknotes needed
+# Calculate the number of 10€ banknotes needed
 if amount >= 10:
     banknotes += amount // 10
     amount = amount % 10
-#Calculate the number of 5€ banknotes needed
+# Calculate the number of 5€ banknotes needed
 if amount >= 5:
     banknotes += amount // 5
     amount = amount % 5
-#Calculate the number of 1€ banknotes needed
+# Calculate the number of 1€ banknotes needed
 if amount >= 1:
     banknotes += amount // 1
     amount = amount % 1
-#Output of banknotes
+# Output of banknotes
 print(f"Amount of banknotes needed: {banknotes}")
