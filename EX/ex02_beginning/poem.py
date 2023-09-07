6638291daf590535caeb3028fb7d7ec57d2896b8
Tanel Paal <tanpaa@taltech.ee>
2023-09-07 10:19:12 +0300

    Try1

--- EX/ex02_beginning/poem.py
+++ EX/ex02_beginning/poem.py
@@ -18,4 +18,5 @@ And so will you!
 color = input("Enter a color: ")
 objects = input("Enter a plural noun: ")
 activity = input("Enter an activity: ")
-print(f"Roses are {color}, \n{objects} are blue, \nI love to {activity}, \nAnd so will you!")
+
+print(f"Roses are {color}\n{objects} are blue\nI love to {activity}\nAnd so will you!")
