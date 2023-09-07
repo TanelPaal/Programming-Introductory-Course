867a4032a57b00634540c521d8c19d4f3a98847f
Tanel Paal <tanpaa@taltech.ee>
2023-09-07 10:19:12 +0300

    Try1

--- EX/ex02_beginning/poem.py
+++ EX/ex02_beginning/poem.py
@@ -3,7 +3,6 @@
 """
 Ask for user's inputs and print out a poem in one line using f string and newline (\n)
 
-
 Example input:
 color = "red"
 objects = "violets"
@@ -18,4 +17,5 @@ And so will you!
 color = input("Enter a color: ")
 objects = input("Enter a plural noun: ")
 activity = input("Enter an activity: ")
-print(f"Roses are {color}, \n{objects} are blue, \nI love to {activity}, \nAnd so will you!")
+
+print(f"Roses are {color}\n{objects} are blue\nI love to {activity}\nAnd so will you!")
