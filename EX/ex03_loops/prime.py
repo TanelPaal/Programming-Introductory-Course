f5acd84932a6c3ce2422aac254fcce3091046bde
Pyriand <ptan02@gmail.com>
2023-09-17 20:02:26 +0300

    Test1

--- EX/ex03_loops/prime.py
+++ EX/ex03_loops/prime.py
@@ -14,15 +14,11 @@ def is_prime_number(number: int) -> bool:
     :param number: the number to check.
     :return: boolean True if number is a prime number or False if number is not a prime number.
     """
-    if number <= 1:
-        return False
-    if number <= 3:
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    else:
         return True
-    # Check for divisibility by 2 or 3.
-    # If the number is divisible by either 2 or 3, it cannot be a prime number.
-    if number % 2 == 0 or number % 3 == 0:
-        return False
-    # Check the divisibility by numbers of the form 6k ± 1, where k is an integer
 
 
 if __name__ == '__main__':
@@ -32,5 +28,4 @@ if __name__ == '__main__':
     print(is_prime_number(4))  # -> False
     print(is_prime_number(7))  # -> True
     print(is_prime_number(88))  # -> False
-    print(is_prime_number(5))
-    print(is_prime_number(6))
+    print(is_prime_number(174440041))  # -> True (Slow at finding the answer)
