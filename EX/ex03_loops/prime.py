107a6dd5bb2eeef6acf11bbb7f516ce05a543e5a
Pyriand <ptan02@gmail.com>
2023-09-17 20:08:54 +0300

    Test1

--- EX/ex03_loops/prime.py
+++ EX/ex03_loops/prime.py
@@ -30,4 +30,5 @@ if __name__ == '__main__':
     print(is_prime_number(4))  # -> False
     print(is_prime_number(7))  # -> True
     print(is_prime_number(88))  # -> False
+    print(is_prime_number(1))  # -> False
     print(is_prime_number(174440041))  # -> True (Takes too long to find if it's a prime number)
