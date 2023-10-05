7087b8975c0610f1f6c3bc54dd2984dbb245da11
Tanel Paal <tanpaa@taltech.ee>
2023-10-05 10:14:57 +0300

    Try1

--- EX/ex05_lists/lists.py
+++ EX/ex05_lists/lists.py
@@ -92,9 +92,8 @@ def search_by_model(all_phones: str, model_to_search: str) -> list:
 
     # Iterate through each phone in the list of phones.
     for phone in list_of_phones(all_phones):
-        model = ' '.join(phone.split(' ')[1:])  # Extract the model (the part after the first space).
-        if search_term in model.lower():
-            results.append(model)  # Add the phone to the results list if it matches and is not already present.
+        if search_term in phone.lower().split(' ')[1:]:
+            results.append(phone)  # Add the phone to the results list if it matches and is not already present.
     return results  # Return the list of matching phones (brand and model).
 
 
