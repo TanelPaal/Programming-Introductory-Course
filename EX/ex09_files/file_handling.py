08776f1de9628a57add7ba0cc67f7c3403b255ef
Pyriand <ptan02@gmail.com>
2023-11-03 17:08:23 +0200

    Test1

--- EX/ex09_files/file_handling.py
+++ EX/ex09_files/file_handling.py
@@ -73,7 +73,8 @@ def write_contents_to_file(filename: str, contents: str) -> None:
     :param contents: The content to write to the file.
     :return: None
     """
-    pass
+    with open(filename, 'w') as file:
+        file.write(contents)
 
 
 def write_lines_to_file(filename: str, lines: list[str]) -> None:
