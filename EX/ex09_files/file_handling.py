0aa629381e458cf9b85935c00046e166b629c61c
Pyriand <ptan02@gmail.com>
2023-11-05 01:57:25 +0200

    Test1

--- EX/ex09_files/file_handling.py
+++ EX/ex09_files/file_handling.py
@@ -204,5 +204,68 @@ def merge_dates_and_towns_into_csv(dates_filename: str, towns_filename: str, csv
                 csv_writer.writerow([name, merged_data[name]['town'], merged_data[name]['date']])
 
 
+def read_csv_file_into_list_of_dicts(filename: str) -> list[dict[str, str]]:
+    """
+    Read a CSV file into a list of dictionaries.
+
+    The header line of the CSV file will be used as keys for the dictionaries.
+    If there are only headers or no rows in the CSV file, the result is an empty list.
+    Each line after the header line will result in a dictionary inside the result list.
+    Every line should contain the same number of fields.
+    The order of the elements in the list corresponds to the lines in the file
+    (the first line becomes the first element, and so on).
+
+    Given a CSV file like this:
+    name,age,sex
+    John,12,M
+    Mary,13,F
+
+    The result will be:
+    [
+      {"name": "John", "age": "12", "sex": "M"},
+      {"name": "Mary", "age": "13", "sex": "F"},
+    ]
+
+    :param filename: The name of the CSV file to read.
+    :return: A list of dictionaries where keys are taken from the header and values are strings.
+    """
+    result = []
+    with open(filename, 'r', newline='', encoding="utf-8") as file:
+        # Use DictReader to directly read the file into a dictionary
+        csv_reader = csv.DictReader(file)
+        # Convert each row in the reader to a dictionary and append to result list
+        for row in csv_reader:
+            # Ensure that all values are strings as required
+            result.append({key: str(value) for key, value in row.items()})
+    return result
+
+
+def write_list_of_dicts_to_csv_file(filename: str, data: list[dict]) -> None:
+    """
+    Write a list of dictionaries to a CSV file.
+
+    Each dictionary in the 'data' list represents a row in the CSV file,
+    with dictionary keys representing the fields of a header.
+    Fields missing in certain rows will be empty in these rows.
+    The order of rows in the file matches the order of elements in the list.
+
+    Example data:
+    [
+      {"name": "john", "age": "12"},
+      {"name": "mary", "town": "London"}
+    ]
+
+    This data will be written to the CSV file as:
+    name,age,town
+    john,12,
+    mary,,London
+
+    :param filename: The name of the file to write to.
+    :param data: List of dictionaries to write to the file.
+    :return: None
+    """
+    pass
+
 if __name__ == '__main__':
-    merge_dates_and_towns_into_csv(dates_filename="dates.csv", towns_filename="towns.csv", csv_output_filename="output.csv")
+    #  merge_dates_and_towns_into_csv(dates_filename="dates.csv", towns_filename="towns.csv", csv_output_filename="output.csv")
+    print(read_csv_file_into_list_of_dicts(filename="input.csv"))
