f5379229844a44eb6bbd4fc80985950137a7d528
Tanel Paal <tanpaa@taltech.ee>
2023-11-06 12:10:59 +0200

    Try1

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,43 +1,53 @@
-"""WAT. """
+"""WAT."""
 
 
 def function_a(x: int) -> int:
+    """Docstring."""
     result = x / x
     return result
 
 
 def function_b(x: int) -> int:
+    """Docstring."""
     result = x - 11
     return result
 
 
-def function_c(x):
+def function_c(x: int) -> int:
+    """Docstring."""
     return x
 
 
-def function_d(x):
+def function_d(x: int) -> int:
+    """Docstring."""
     return x
 
 
-def function_e(x):
+def function_e(x: int) -> int:
+    """Docstring."""
     return x
 
 
-def function_f(x):
+def function_f(x: int) -> int:
+    """Docstring."""
     return x
 
 
-def function_g(x):
+def function_g(x: int) -> int:
+    """Docstring."""
     return x
 
 
-def function_h(x):
+def function_h(x: int) -> int:
+    """Docstring."""
     return x
 
 
-def function_i(x):
+def function_i(x: int) -> int:
+    """Docstring."""
     return x
 
 
-def function_j(x):
+def function_j(x: int) -> int:
+    """Docstring."""
     return x
