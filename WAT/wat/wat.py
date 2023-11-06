8ca798070b6db28e43a5f2358ca5d26947191396
Tanel Paal <tanpaa@taltech.ee>
2023-11-06 12:06:35 +0200

    Try1

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,3 +1,42 @@
 """WAT. """
+
+
 def function_a(x):
+    result = x / x
+    return result
+
+
+def function_b(x):
+    return x
+
+
+def function_c(x):
+    return x
+
+
+def function_d(x):
+    return x
+
+
+def function_e(x):
+    return x
+
+
+def function_f(x):
+    return x
+
+
+def function_g(x):
+    return x
+
+
+def function_h(x):
+    return x
+
+
+def function_i(x):
+    return x
+
+
+def function_j(x):
     return x
